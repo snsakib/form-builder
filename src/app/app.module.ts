58508dcdcb5f2f7af22638e12927814ac8d7b321
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-04-16 12:03:47 +0600

    feat: import ReactiveFormsModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainNavComponent
-  ],
+  declarations: [AppComponent, MainNavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,4 +31,4 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
