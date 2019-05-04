2075f9d7f8daff16f651009aeea3cc2de6036d41
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-05-04 23:04:03 +0600

    chore: fix vulnerability

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 echo "Starting script..."
 
+read -r -p "Enter Commit Message: " CM
+
 # Production build
 echo "Building production files..."
 ng build --prod --output-path docs --base-href /form-builder/
@@ -14,6 +16,5 @@ cp index.html 404.html
 # Publishing to GitHub
 echo 'Publishing to GitHub'
 git add --all
-read -r -p "Commit Message: " CM
 git commit -m "$CM"
 git push
